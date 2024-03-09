c9193dd5bdc71db4e319ccee6450872b4228b3a9
Karanjot-singh-ji <114502710+Karanjot-singh-ji@users.noreply.github.com>
2024-03-10 02:28:01 +0530

    Updated

--- signify/app.py
+++ signify/app.py
@@ -40,7 +40,7 @@ def generate_frames(camera):
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
@@ -62,6 +62,13 @@ def get_class_name():
     global current_class_name
     return jsonify({'class_name': current_class_name})
 
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
